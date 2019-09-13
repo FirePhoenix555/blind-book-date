52996d5ad2f8b802303d28ea4e241bf264e5a5ae
bsd <battleshipdata@gmail.com>
2019-09-12 19:44:24 -0500

    removed extra code

--- server.js
+++ server.js
@@ -3,10 +3,5 @@ const server = app.listen(3000);
 const io = socket(server);
 app.use(express.static("public"));
 io.sockets.on("connection", socket => {
-    socket.on("data", data => {
-        // console.log(data);
-        code.push({
-            data
-        });
-    })
+    
 });
