d517012030e2a95f0b28e237757f63277b42b7b7
bsd <battleshipdata@gmail.com>
2019-09-14 10:26:53 -0500

    added data submission

---
+++ public/main.js
@@ -0,0 +1,198 @@
+const socket = io();
+
+let lob;
+let waiting = true;
+
+let ct = 0;
+
+/*
+get a list of books
+*/
+
+socket.on("books", data => {
+	if (ct != 0) return;
+	lob = data;
+	waiting = false;
+	ct++;
+})
+
+// https://davidwalsh.name/query-string-javascript
+function getUrlParameter(name) {
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    var results = regex.exec(location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+}
+
+// /book
+function checkRead() {
+	if (document.getElementById("agree").checked) {
+		location.href = "/read-book?num=" + getUrlParameter("num");
+	}
+}
+
+function getSpecifics() {
+	// genre, description, pages, series
+	if (waiting) {
+		setTimeout(getSpecifics, 20);
+		return;
+	}
+
+	let b = lob[getUrlParameter("num")];
+
+	document.getElementById("book-genre").textContent = b.genre;
+	document.getElementById("book-description").textContent = b.description;
+	document.getElementById("pages").textContent = b.pages;
+	document.getElementById("series").textContent = ((b.series) ? "Yes" : "No");
+}
+
+// /read-book
+function getRSpecifics() {
+	// genre, description, pages, series
+	if (waiting) {
+		setTimeout(getRSpecifics, 20);
+		return;
+	}
+
+	let b = lob[getUrlParameter("num")];
+
+	document.getElementById("title").textContent = b.title;
+	document.getElementById("author").textContent = b.author;
+	document.getElementById("coverImg").setAttribute("src", b["cover-img"]);
+}
+
+// /find-book
+function goToBook(num) {
+	// get specific book
+	location.href = "/book?num=" + num;
+}
+
+function searchByGenre(genre) {
+	if (waiting) {
+		setTimeout(() => {
+			searchByGenre(genre);
+		}, 20);
+		return;
+	}
+
+	let options = [];
+	for (let i = 0; i < lob.length; i++) {
+		if (lob[i].genre == genre || genre == "ALL") {
+			options.push(lob[i]);
+		}
+	}
+
+	document.getElementById("search-type").textContent = genre;
+
+	// description.substring(0, 20) + "...";
+	let arr = makeArr(options);
+	makeTable(document.getElementById("books"), arr);
+
+	// put them onto the webpage
+	console.log(options);
+}
+
+function substringIze(desc, len) {
+	let str = desc;
+	if (desc.length > len) {
+		str = desc.substring(0, len) + "...";
+	}
+	return str;
+}
+
+function makeArr(options) {
+	let arr = [
+		[
+			"Genre",
+			"Description"
+		]
+	];
+
+	for (let i = 0; i < options.length; i++) {
+		let a = {};
+		a["book-genre"] = options[i].genre;
+		a["book-description"] = substringIze(options[i].description);
+
+		arr.push(a);
+	}
+
+	return arr;
+}
+
+function makeTable(elt, arr) {
+	// [
+	// 	[
+	// 		"Genre",
+	// 		"Description"
+	// 	],
+	// 	{
+	// 		"book-genre": "A",
+	// 		"book-description": "A wonderful thing"
+	// 	}
+	// ]
+
+	let table = document.createElement("table");
+
+	let t = document.createElement("tr");
+	for (let i = 0; i < arr[0].length; i++) {
+		let th = document.createElement("th");
+		th.textContent = arr[0][i];
+		t.appendChild(th);
+	}
+	table.appendChild(t);
+
+
+	for (let i = 1; i < arr.length; i++) {
+		let tr = document.createElement("tr");
+		tr.setAttribute("class", "book");
+		tr.setAttribute("onclick", "goToBook(" + (i - 1) + ")");
+
+		let ok = Object.keys(arr[i]);
+		for (let j = 0; j < ok.length; j++) {
+			let td = document.createElement("td");
+			let span = document.createElement("span");
+			span.setAttribute("class", ok[j]);
+			span.textContent = arr[i][ok[j]];
+			td.appendChild(span);
+			tr.appendChild(td);
+		}
+
+		table.appendChild(tr);
+	}
+
+	while (elt.firstChild) {
+		elt.removeChild(elt.firstChild);
+	}
+
+	elt.appendChild(table);
+}
+
+// /add-book
+function submit() {
+	let obj = {};
+
+	let genre = document.getElementById("genre-select").value;
+	if (genre == "new") genre = document.getElementById("new-genre").value;
+	if (genre == "") alert("You need a genre!");
+	obj.genre = genre;
+
+	obj.pages = document.getElementById("pages").value;
+	if (obj.pages == "") alert("You need a page count!");
+
+	obj.series = document.getElementById("series").checked;
+
+	obj.description = document.getElementById("book-description").value;
+	if (obj.description == "") alert("You need a description!");
+
+	obj.title = document.getElementById("title").value;
+	if (obj.title == "") alert("You need a title!");
+
+	obj.author = document.getElementById("author").value;
+	if (obj.author == "") alert("You need an author!");
+
+	obj["cover-img"] = document.getElementById("cvr-img").value;
+	if (obj["cover-img"] == "") alert("You need a cover image!");
+
+	socket.emit("new-book", obj);
+	location.href = "/find-book";
+}
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 /* TODO
-add actual data submission
+add in the classroom
 */
 
 require("dotenv").config();
@@ -18,7 +18,17 @@ app.use("/book", express.static("public/book.html"));
 app.use("/read-book", express.static("public/reading.html"));
 
 io.sockets.on("connection", socket => {
-    
+	console.log("New socket!");
+
+	if (received) {
+		socket.emit("books", bks);
+	}
+
+	socket.on("new-book", data => {
+		console.log("New book! " + data.title)
+		bks.push(data);
+		books.set(bks);
+	})
 });
 
 const firebaseConfig = {
@@ -36,16 +46,34 @@ firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
 const books = database.ref("books");
 let keys = [];
+let bks = [];
+
+let received = false;
 
 books.on("value", data => {
-    keys = [];
+	keys = [];
+	bks = [];
     let data2 = data.val();
     if (!data2) return;
     let dkeys = Object.keys(data2);
     for (let i = 0; i < dkeys.length; i++) {
         let key = dkeys[i];
-        keys.push(key);
-    }
+		keys.push(key);
+		bks.push(data2[key]);
+	}
+
+	received = true;
+	io.sockets.emit("books", bks);
 });
+
+// books.child("0").set({
+// 	title: "The Giver",
+// 	author: "Lois Lowry",
+// 	genre: "Science Fiction",
+// 	series: true,
+// 	pages: 240,
+// 	description: "The test book. guess what",
+// 	"cover-img": "https://images-na.ssl-images-amazon.com/images/I/51fRar1PvAL._SX300_BO1,204,203,200_.jpg"
+// });
 
 console.log("Running...");
